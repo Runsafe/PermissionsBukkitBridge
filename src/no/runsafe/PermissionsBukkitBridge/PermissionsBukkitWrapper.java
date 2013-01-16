aa5ecd95822b05a7f53b360ae562bd6948607ef7
Morten Nilsen <morten@runsafe.no>
2013-01-16 13:20:35 +0100

    Hook permissions to check builder rights

--- src/no/runsafe/PermissionsBukkitBridge/PermissionsBukkitWrapper.java
+++ src/no/runsafe/PermissionsBukkitBridge/PermissionsBukkitWrapper.java
@@ -2,14 +2,16 @@ package no.runsafe.PermissionsBukkitBridge;
 
 import com.platymuus.bukkit.permissions.Group;
 import com.platymuus.bukkit.permissions.PermissionsPlugin;
+import no.runsafe.framework.hook.IPlayerBuildPermission;
 import no.runsafe.framework.hook.IPlayerPermissions;
 import no.runsafe.framework.plugin.PluginResolver;
+import no.runsafe.framework.server.RunsafeLocation;
 import no.runsafe.framework.server.player.RunsafePlayer;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PermissionsBukkitWrapper implements IPlayerPermissions
+public class PermissionsBukkitWrapper implements IPlayerPermissions, IPlayerBuildPermission
 {
 	public PermissionsBukkitWrapper(PluginResolver pluginResolver)
 	{
@@ -19,17 +21,23 @@ public class PermissionsBukkitWrapper implements IPlayerPermissions
 	@Override
 	public List<String> getUserGroups(RunsafePlayer player)
 	{
-		if(!hasPermissionsBukkit())
+		if (!hasPermissionsBukkit())
 			return null;
 		ArrayList<String> results = new ArrayList<String>();
-		for(Group group : permissionsPlugin.getGroups(player.getName()))
+		for (Group group : permissionsPlugin.getGroups(player.getName()))
 			results.add(group.getName());
 		return results;
 	}
 
+	@Override
+	public boolean blockPlayerBuilding(RunsafePlayer player, RunsafeLocation location)
+	{
+		return !player.hasPermission("permissions.build");
+	}
+
 	private boolean hasPermissionsBukkit()
 	{
-		if(permissionsPlugin == null)
+		if (permissionsPlugin == null)
 			permissionsPlugin = getPermissionsBukkit();
 		return permissionsPlugin != null;
 	}
